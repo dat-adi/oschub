92ab48425db89120123e4e0d1c2606338163870b
dat-adi <dat.adithya@gmail.com>
2020-09-19 23:53:24 +0530

    Log Out feature added to home page

--- event_regis/views.py
+++ event_regis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from .forms import EventForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, CreateView,
                                   ListView, DetailView,
                                   UpdateView, DeleteView)
@@ -8,35 +9,35 @@ from . import models
 
 
 # Listing out all the events
-class EventListView(ListView):
+class EventListView(LoginRequiredMixin, ListView):
     context_object_name = 'events'
     model = models.Event
 
 
 # Home page for the event access
-class EventHomeView(TemplateView):
+class EventHomeView(LoginRequiredMixin, TemplateView):
     template_name = 'event_regis/event_home_page.html'
 
 
 # Shows the details of the event
-class EventDetailView(DetailView):
+class EventDetailView(LoginRequiredMixin, DetailView):
     model = models.Event
 
 
 # Addition of the event
-class RegisterEvent(CreateView):
+class RegisterEvent(LoginRequiredMixin, CreateView):
     fields = ('event_name', 'event_start_time', 'event_details', 'live_stream_link', 'documentation_link')
     model = models.Event
 
 
 # Updating the event
-class EventUpdateView(UpdateView):
+class EventUpdateView(LoginRequiredMixin, UpdateView):
     redirect_field_name = 'event_regis/event_detail.html'
     form_class = EventForm
     model = models.Event
 
 
 # Deleting the event
-class EventDeleteView(DeleteView):
+class EventDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Event
     success_url = reverse_lazy('event_regis:list')
