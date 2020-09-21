98ae1ca19e4f501f11954810fd4c1b8c57c82bb4
dat-adi <dat.adithya@gmail.com>
2020-09-21 20:50:18 +0530

    fixed issue with livestream_event.html

--- livestream_app/views.py
+++ livestream_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from event_regis import models
 
 
-class LiveStreamView(TemplateView):
+class LiveStreamView(DetailView):
     template_name = "livestream_app/livestream_event.html"
+    model = models.Event
 
 # TODO: iframe src needs to be able to take in from event components.
